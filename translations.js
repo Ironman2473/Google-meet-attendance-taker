95ea0d16dbbd7a3570746bb8cd25f408a7b0e0ea
Ironman2473 <67648813+Ironman2473@users.noreply.github.com>
2020-12-13 17:21:48 +0530

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,16 @@
+const translations = {
+    en: {
+        title: 'Attendance for Google Meet',
+        version: 'Version',
+        attribution: 'By Tyler Lin and Adit Bala',
+        exporting: 'Exporting',
+        areYouSure: 'Are you sure you want to delete the class'
+    },
+    fr: {
+        title: 'Appel pour Google Meet',
+        version: 'Version',
+        attribution: 'De Tyler Lin et Adit Bala',
+        exporting: 'Exportant',
+        areYouSure: 'idk bruh'
+    }
+}
